dc68857240f1f50062b39e3753057699ae87d371
nickoarturo81 <nicolas.arturo@campusucc.edu.co>
2025-02-14 01:22:31 +0000

    Ejercicios Clase 2

---
+++ clase2/Bucles.java
@@ -0,0 +1,22 @@
+public class Bucles {
+    
+    public static void main (String[] args){
+
+        //for(inicio; condición; incremento o decremento)
+        for(int i = 1; i<=5; i++){
+            System.out.println("Numero de Iteración: "+ i);
+        }
+
+        /*Estructura del While
+            inicio
+            while(condición){
+            incremento o decremento}
+        */
+
+int j = 0;
+while (j <= 5) {
+    System.out.println("Número de Iteración: " + j);
+    j++;
+}
+    }
+}
---
+++ clase2/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    //Método main
+    public static void main (String[] args){
+
+
+        //Tipos de datos
+            //Enteros = int(Datos cortos) y long(Datos Extensos)
+        int edad = 25;
+            //Reales = float (Almacena rangos de valores pequeños), double(Almacena rangos de valores mas grandes) 
+        double altura = 1.75;
+            //Cadenas
+        String nombre = "Juan";
+            //Logicos = Verdadero o Falso
+        boolean esEstudiante = true;
+        //Concatenación = Es concatenar un texto con una variable
+        System.out.println("Nombre: " + nombre + ", Edad: " + edad + ", Altura: " + altura + ", Estudiante: " + esEstudiante);
+    }
+}
