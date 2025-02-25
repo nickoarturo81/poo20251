58d7bf2393e88cb5bc6e113508292519569dbab5
nickoarturo81 <nicolas.arturo@campusucc.edu.co>
2025-02-25 20:00:49 +0000

    Adición del 2do Arreglo Unidimensional

--- Desarrollo_Taller/Ejercicio4.java
+++ Desarrollo_Taller/Ejercicio4.java
@@ -6,19 +6,27 @@ public class Ejercicio4 {
          * se pide hallar el valor mayor.
          */
 
-         //Creación del arreglo unidimensional
-         int[] a = {10, 50, 47, 26, 18, 32, 15};
+         //Creación del 1er arreglo unidimensional
+         int[] a = {10, 50, 31, 26, 18, 32, 15};
+
+         //Creación del 2do arreglo unidimensional
+         int[] b = {20, 31, 17, 42, 53, 60, 61};
          
-         /*
-          * Creación de variable edadMayor que empiece a contar desde la posición 0, por lo que empezará
-          * a contar a partir en este caso el número 10.
+         /*Variable para iniciar con el arreglo a[10], la variable toma el valor 10
           */
          int edadMayor = a[0];
 
-         //Identificar Valor Mayor
+         //Identificar Valor Mayor 1er Arreglo
          for (int i = 0; i < a.length; i++) {
-            if (a[i] > edadMayor) {
-                    edadMayor = a[i];
+            if (a[i] > edadMayor) { //--> Compara si el arreglo a con X posición es Mayor a la valor de la variable
+                    edadMayor = a[i]; //--> la variable toma el valor si este es mayor.
+            }
+         }
+
+         //Identificar Valor Mayor 2do Arreglo
+         for (int i = 0; i < b.length; i++) {
+            if (b[i] > edadMayor) {
+                    edadMayor = b[i];
             }
          }
 
