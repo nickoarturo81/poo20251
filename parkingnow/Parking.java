187edf8bdddead9872f6470506fb2160a8d13922
nickoarturo81 <nicolas.arturo@campusucc.edu.co>
2025-03-12 02:16:28 +0000

    Continuación: Se ha agregado codigo para ubicar la placa y solicitar el tiempo, y liberar la posición una vez la motocicleta ha sido retirada y corregir errores.

--- parkingnow/Parking.java
+++ parkingnow/Parking.java
@@ -37,43 +37,91 @@ public class Parking {
                     leer.nextLine();
 
                     int posicion;
-                    
+                    //Acomodar motocicleta de cilindraje <400
                     if (cilindraje <= 400){
                         System.out.println("Elija la posición (0-19): ");
                         posicion = leer.nextInt();
                     if (posicion >= 0 && posicion < 20 && a[posicion] == null){
                         a[posicion] = placa;
                         System.out.println("Motocicleta con placa " + placa + " registrada en posicion " + posicion);
-                    }else {
-                        System.out.println("Ocupado");
+                    }else if (posicion >=20){
+                        System.out.println("No existe posición");
+                    } else {
+                        System.out.println("No disponible");
                     }
-                }
-                leer.nextLine();
-                System.out.println();
+                   }
+                   //Acomodar motocicleta de cilindraje >400
+                   if (cilindraje > 400){
+                    System.out.println("Elija la posición (0-9): ");
+                    posicion = leer.nextInt();
+                    if (posicion >= 0 && posicion < 9 && b[posicion] == null){
+                    b[posicion] = placa;
+                    System.out.println("Motocicleta con placa " + placa + " registrada en posicion " + posicion);
+                    }else {
+                    System.out.println("Ocupado");
+                    }
+                    }
+                    leer.nextLine();
+                    System.out.println();
                 break;
 
                 case 2:
                     System.out.print("*** REGISTRAR SALIDA ***");
                     System.out.println();
                     System.out.print("Buscar PLACA: ");
-                    String placa1 = leer.nextLine();
                     leer.nextLine();
-                    System.out.println();
-                    System.out.print("Registrar TIEMPO: ");
-                    int tiempo = leer.nextInt();
-                    
-                    if (tiempo<=30){
-                         System.out.print( "Valor a pagar: $0 ");
-                    } else if (tiempo >30 && tiempo <=60){
-                        System.out.print( " Valor a pagar es: $800");
-                    } else {
-                        System.out.println("Valor a pagar es: $2000");
+                    String placa1 = leer.nextLine();
+                    boolean encontrado = false;
+                    int posicionPlaca = -1;
+                    String cilindrajeMoto = "";
+                    //buscar placa menores a 400cc
+                    for (int i = 0; i < a.length; i++){
+                        if (a[i] != null && a[i].equals(placa1)){
+                            encontrado = true;
+                            posicionPlaca = i;
+                            cilindrajeMoto = "menor de 400cc";
+                            break;
+                        }
                     }
-                break;    
-            } 
-                    
-            
+                    //buscar placa mayores a 400cc
+                    if (!encontrado) {
+                        for (int i = 0; i < b.length; i++) {
+                            if (b[i] != null && b[i].equals(placa1)){
+                                encontrado = true;
+                                posicionPlaca = i;
+                                cilindrajeMoto = "mayor de 400cc";
+                                break;
+                            }
+                        }
+                    }
+                    //Al ubicar la placa, solicitar el tiempo
+                    if (encontrado){
+                        System.out.print("Registrar TIEMPO: ");
+                        int tiempo = leer.nextInt();
+                        if (tiempo<=30){
+                            System.out.print( "La moto con placa: " + placa1 + " ubicado en la posición: " + posicionPlaca + " tiene un valor a pagar: $0 ");
+                            System.out.println();
+                       } else if (tiempo >30 && tiempo <=60){
+                           System.out.print( "La moto con placa: " + placa1 + " ubicado en la posición: " + posicionPlaca + " tiene un valor a pagar: $800 ");
+                           System.out.println();
+                       } else if (tiempo >60){
+                           System.out.println("La moto con placa: " + placa1 + " ubicado en la posición: " + posicionPlaca + " tiene un valor a pagar: $2000 ");
+                           System.out.println();
+                       }
 
+                       //Liberar posición al dar salida moto
+                       if (cilindrajeMoto.equals("menor de 400cc")){
+                        a[posicionPlaca] = null;
+                       } else if (cilindrajeMoto.equals("mayor de 400cc")) {
+                        b[posicionPlaca] = null;
+                       }
+                       System.out.println("La posición" + posicionPlaca + " ha sido liberado");
+
+                    } else {
+                        System.out.println("La placa" + placa1 + " no ha sido registrada");
+                    }
+                break;
+            } 
         } while (opc != 3);
     }
 }
