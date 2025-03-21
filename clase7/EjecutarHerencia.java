998021713a528829d23df52dafa2477142c7cd49
nickoarturo81 <nicolas.arturo@campusucc.edu.co>
2025-03-21 00:52:50 +0000

    Se creo la clase EjercutarHerencia

---
+++ clase7/EjecutarHerencia.java
@@ -0,0 +1,14 @@
+package clase7;
+
+public class EjecutarHerencia {
+    
+    public static void main(String[] args) {
+        
+        //Creación de los Objetos (Instanciar)
+        Trabajador objT1 = new Operario(1, "Pepito", "Puertas", 135.0);
+        Trabajador objT2 = new Consultor(2, "Juan", "Islas", 10);
+
+        System.out.println(objT1);
+        System.out.println(objT2);
+    }
+}
