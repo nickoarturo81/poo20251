590cbc5c82a20471242d4802047182cf9f886b8f
nickoarturo81 <nicolas.arturo@campusucc.edu.co>
2025-03-28 00:05:13 +0000

    Creación Clase 8: Agregación

---
+++ clase8/Cliente.java
@@ -0,0 +1,17 @@
+package clase8;
+
+public class Cliente {
+
+    private long cedula;
+    private String nombre;
+
+    public Cliente(long cedula, String nombre) {
+        this.cedula = cedula;
+        this.nombre = nombre;
+    }
+
+    public String toString() {
+        return "Cliente { Cedula: " + cedula + " Nombre: " + nombre + "}";
+    }
+
+}
---
+++ clase8/Cuenta.java
@@ -0,0 +1,19 @@
+package clase8;
+
+public class Cuenta {
+
+    private int numero;
+    private String tipo;
+    private double saldo;
+
+    public Cuenta(int numero, String tipo, double saldo) {
+        this.numero = numero;
+        this.tipo = tipo;
+        this.saldo = saldo;
+    }
+
+    public String toString() {
+        return "Cuenta { Nombre: " + numero + " tipo: " + tipo + "saldo: " + saldo + "}";
+    }
+
+}
