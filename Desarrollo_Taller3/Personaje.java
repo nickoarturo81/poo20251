4d758942c982fb15761f8642c8c207cba4b6b77c
nickoarturo81 <nicolas.arturo@campusucc.edu.co>
2025-05-14 16:37:47 +0000

    Creación del trabajo final poo llamado Desarrollo_Taller3, se crea clase "Personaje", se crea los atributos y constructores

--- Desarrollo_Taller3/Personaje.java
+++ Desarrollo_Taller3/Personaje.java
@@ -1,21 +1,19 @@
-package JuegoLucha;
+package Desarrollo_Taller3;
 
-import java.util.random.*;
+import java.util.Random;
 import java.util.Scanner;
 
 public class Personaje {
     
-    //Atirbutos:
-    private String nombre:
+    //Atributos:
+    private String nombre;
     private int puntosDeVida;
     private final int MAX_DANO = 30;
     private final int MIN_DANO = 10;
 
-    //Constructores
+    //Constructor
     public Personaje(String nombre, int puntosDeVida, int MAX_DANO, int MIN_DANO){
         this.nombre = nombre;
         this.puntosDeVida = puntosDeVida;
-        this.MAX_DANO = MAX_DANO;
-        this.MIN_DANO = MIN_DANO;
     }
 }
