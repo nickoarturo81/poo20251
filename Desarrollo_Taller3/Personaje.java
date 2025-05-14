fc1630b242b675dfc31dfe2bd52c7cd7fc02443c
nickoarturo81 <nicolas.arturo@campusucc.edu.co>
2025-05-14 17:02:56 +0000

    Se adiciona los métodos

--- Desarrollo_Taller3/Personaje.java
+++ Desarrollo_Taller3/Personaje.java
@@ -12,8 +12,35 @@ public class Personaje {
     private final int MIN_DANO = 10;
 
     //Constructor
-    public Personaje(String nombre, int puntosDeVida, int MAX_DANO, int MIN_DANO){
+    public Personaje(String nombre){
         this.nombre = nombre;
-        this.puntosDeVida = puntosDeVida;
+        this.puntosDeVida = 100;
     }
+
+    //Métodos
+    public String getNombre() {
+        return this.nombre;
+    } // <-- Retorna el nombre del personaje
+
+    public boolean estaVivo(){
+        return this.puntosDeVida > 0;
+    } // < -- Verifica si el personaje está vivo
+
+    public void recibirDano (int dano){
+        this.puntosDeVida = dano;
+        if (this.puntosDeVida < 0){
+            this.puntosDeVida -= 0; // <-- No se puede tener menos de 0 HP
+        }
+    } // < -- Recibir Daño
+
+    public void atacar(Personaje oponente){
+        Random rand = new Random();
+        int dano = rand.nextInt (( MAX_DANO - MIN_DANO ) + 1) + MIN_DANO ;
+        oponente.recibirDano ( dano ) ;
+        System.out.println( this.nombre + " ataca a " + oponente.getNombre() + " causando " + dano + " puntos de daño .") ;
+    } // <-- Realizar un ataque al oponente
+
+    public int getPuntosDeVida(){
+        return this.puntosDeVida;
+    } // < -- Retorna los puntos de vida actuales
 }
