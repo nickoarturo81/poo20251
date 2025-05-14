babc057829070ffadf3f626f6766a36419b03956
nickoarturo81 <nicolas.arturo@campusucc.edu.co>
2025-05-14 20:18:26 +0000

    Se crea clase JuegoLucha, se configura los atributos y metodos

---
+++ Desarrollo_Taller3/JuegoLucha.java
@@ -0,0 +1,46 @@
+package Desarrollo_Taller3;
+
+public class JuegoLucha {
+    
+    //Atributos
+    private Personaje jugador1;
+    private Personaje jugador2;
+
+    //Constructor
+    public JuegoLucha(String nombre1, String nombre2) {
+        this.jugador1 = new Personaje (nombre1);
+        this.jugador2 = new Personaje (nombre2);
+    }
+
+    //Métodos
+    public void iniciarPelea() {
+        System.out.println("Pelea entre: " + jugador1.getNombre() + " VS " + jugador2.getNombre() + "..."); 
+        System.out.println("Round One ¡Fight!");
+
+        while (jugador1.estaVivo() && jugador2.estaVivo()) {
+            turno(jugador1, jugador2);
+            if (jugador2.estaVivo()){
+                turno(jugador2, jugador1);
+            }
+        }
+
+        if (jugador1.estaVivo()){
+            System.out.println(jugador1.getNombre() + " ha ganado ");
+        } else {
+            System.out.println(jugador2.getNombre() + "ha ganado ");
+        }
+
+    }
+
+    private void turno (Personaje atacante, Personaje defensor) {
+        System.out.println("Turno de " + atacante.getNombre() + " Puntos de vida de " + defensor.getNombre() + ": " + defensor.getPuntosDeVida());
+        atacante.atacar(defensor);
+        System.out.println(defensor.getNombre() + " ahora tiene " + defensor.getPuntosDeVida() + " puntos de vida ");
+
+    }
+
+    
+    
+}
+
+
